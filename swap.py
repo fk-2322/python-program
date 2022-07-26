83ea3ad9632744b05469bab1b26b03eb28acf8b2
fk-2322 <42044097+fk-2322@users.noreply.github.com>
2022-07-26 20:54:04 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,27 @@
+
+# a = 5
+# b = 10
+# c = a
+# a = b
+# b = c
+# print(a)
+# print(b)
+
+from math import factorial
+
+
+
+#x = 5
+#for i in range(1,6):
+ #   print(i)
+
+f =3
+x=1
+for i in range (1,f+1):
+    x=x*i
+
+print(x)
+
+
+
+
